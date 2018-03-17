d335ae30365d9f3a1a8d4c6bb0b40b4bce0e729e
TerrenceChan <roxes2112@yahoo.com>
2018-03-17 18:14:50 +0800

    *Created custom collision detection with raycasts.
    *Moving left and right with the treant.
    
    *[TODO] To fix cumulative velocity from gravity variable.

---
+++ Tingleblast/Assets/Scripts/Treant.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TreantMovement))]
+public class Treant : MonoBehaviour
+{
+    float gravity = -20.0f;
+    float moveSpeed = 6.0f;
+    Vector3 velocity;
+
+    TreantMovement treantMovement;
+
+    void Start()
+    {
+        treantMovement = GetComponent<TreantMovement>();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        treantMovement.Move(velocity * Time.deltaTime);
+    }
+}
